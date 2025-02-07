4e117e45547a007409d813d1aa8ba630981ef7df
eunsang <thecynicdog0328@gmail.com>
2025-02-07 12:15:33 +0900

    chore:

---
+++ src/layout/ConfigPopup.jsx
@@ -0,0 +1,55 @@
+import {useTheme, useTranslation} from "../Context";
+import {useQuery} from "react-query";
+import {fetchColumnData} from "../data/APIs";
+import {useCallback, useEffect} from "react";
+
+const ConfigPopup = ({ isOpen, onClose, rowData }) => {
+
+    const {theme} = useTheme();
+    const {t} = useTranslation();
+
+    const {data: columnData, isLoading: isColumnDataLoading} = useQuery(
+        ['columnData', rowData?.COL_NAME],
+        () => fetchColumnData(rowData.COL_NAME),
+        {
+            enabled: !!rowData?.COL_NAME && isOpen,
+        }
+    );
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+
+        return () => {
+            document.body.style.overflow = '';
+        };
+    }, [isOpen]);
+
+    const handleEscKey = useCallback((event) => {
+        if (event.key === 'Escape') {
+            onClose();
+        }
+    }, [onClose]);
+
+    useEffect(() => {
+        if (isOpen) {
+            document.addEventListener('keydown', handleEscKey);
+        }
+        return () => {
+            document.removeEventListener('keydown', handleEscKey);
+        };
+    }, [isOpen]);
+
+    if (!isOpen) return null;
+
+    return (
+        <>
+            POPUP CONTENTS
+        </>
+    )
+}
+
+export default ConfigPopup;
