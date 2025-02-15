277ee1d2052ef795ffab3ef16ebac0e18fa18859
eunsang <thecynicdog0328@gmail.com>
2025-02-15 11:56:23 +0900

    chore:

--- src/context/Popup.jsx
+++ src/context/Popup.jsx
@@ -1,11 +1,17 @@
-import {createContext, useContext, useState} from "react";
+import {createContext, useContext, useEffect, useState} from "react";
+import {useAuth} from "./Auth";
 
 const PopupContext = createContext();
 
 export const PopupProvider = ({ children }) => {
-
     const [isConfigPopupOpen, setIsConfigPopupOpen] = useState(false);
     const [isProjectPopupOpen, setIsProjectPopupOpen] = useState(true);
+
+    const { auth } = useAuth();
+
+    useEffect(() => {
+        setIsProjectPopupOpen(true);
+    }, [auth]);
 
     return (
         <PopupContext.Provider value={{
